e04938e7876c12e1b9d9034b0976b938b85a7b40
Sedrac Lucas Calupeteca <sedraccalupeteca@gmail.com>
2023-09-23 22:59:50 +0100

    modifly null safe and timestamp

--- src/main/kotlin/com/eigframework/core/abs/CommunDetails.kt
+++ src/main/kotlin/com/eigframework/core/abs/CommunDetails.kt
@@ -1,16 +1,16 @@
 package com.eigframework.core.abs
 
-import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.UUID
 
 abstract class CommunDetails(
-    val id: UUID = UUID.randomUUID(),
-    val createdAt: LocalDate = LocalDate.now(),
-    val updatedAt: LocalDate = LocalDate.now(),
-    val deletedAt: LocalDate,
-    val createdBy: UUID,
-    val updatedBy: UUID,
-    val deletedBy: UUID,
+    val id: UUID? = UUID.randomUUID(),
+    val createdAt: LocalDateTime? = LocalDateTime.now(),
+    val updatedAt: LocalDateTime? = LocalDateTime.now(),
+    val deletedAt: LocalDateTime?,
+    val createdBy: UUID?,
+    val updatedBy: UUID?,
+    val deletedBy: UUID?,
     ) {
 
     override fun equals(other: Any?): Boolean = if(other is CommunDetails) id == other.id else false
